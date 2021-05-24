d04ac96384d914fc8d4a6e3fa480c5993938b80d
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-05-24 18:58:05 +0530

    Create Palindrome(number).py

---
+++ Palindrome(number).py
@@ -0,0 +1,14 @@
+number = int(input("Enter the number:\n"))
+temp = number
+reverse = 0
+
+while (number > 0):
+    remainder = number % 10
+    reverse = reverse * 10 + remainder
+    number = number // 10
+
+if (temp == reverse):
+    print("Palindrome")
+
+else:
+    print("Not palindrome")
