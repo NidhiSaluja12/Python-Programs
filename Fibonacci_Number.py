b74eb0d0ea39ab6e44c25f4df0d18213fc2fa2dc
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-05-25 19:01:13 +0530

    Create Fibonacci_Number.py

---
+++ Fibonacci_Number.py
@@ -0,0 +1,22 @@
+def Fibonacci_Number(n):
+
+    F1 = 0
+    F2 = 1
+
+    if n==0:
+        return 0
+
+    elif n<0:
+        print("Enter only positive integer!")
+        return ""
+
+    else:
+        for i in range (1, n):
+            F = F1+F2
+            F1 = F2
+            F2 = F
+        return F2
+
+n = int(input("Enter the position:\n"))
+print(f"The fibonacci number at position {n} is:")
+print(Fibonacci_Number(n))
