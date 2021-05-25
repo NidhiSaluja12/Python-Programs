ee71803a246bdc5c6cb6b7c6405c70e64c0f050d
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-05-25 17:50:37 +0530

    Create Fibonacci_Series.py

---
+++ Fibonacci_Series.py
@@ -0,0 +1,22 @@
+def Fibonacci_Series(n):
+
+    F1 = 0
+    F2 = 1
+    count = 0
+
+    if n<0:
+        print("Enter only positive integers!")
+    elif n==0:
+        print(f"Fibonacci series till {n} position is {F1}")
+
+    else:
+        print(f"Fibonacci series till {n} position is\n ")
+        while count < n:
+            print(F1)
+            F = F1+F2
+            F1 = F2
+            F2 = F
+            count += 1
+
+n = int(input("Enter the number of elements:\n"))
+Fibonacci_Series(n)
