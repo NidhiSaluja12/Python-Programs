911855ac3854a9471e62e595708334f15f465e64
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-09 22:54:08 +0530

    Create CountLowerAndUpperCaseCharacter.py

---
+++ CountLowerAndUpperCaseCharacter.py
@@ -0,0 +1,14 @@
+str = input("Enter your string:\n")
+
+u = 0
+l = 0
+
+for i in str:
+    if i.islower() == True:
+        l+=1
+    if i.isupper()==True:
+        u+=1
+
+print(f"Number of lower case characters are {l}")
+print(f"Number of upper case characters are {u}")
+
