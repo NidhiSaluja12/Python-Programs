562599f3c03bae322bbcd17b37cc71d1cb6dff94
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-11 17:30:24 +0530

    Create If Statements.py

---
+++ If Statements.py
@@ -0,0 +1,20 @@
+is_hot = False
+is_cold= False
+
+if is_hot:
+    print("Hot Day")
+    print("Drink water.")
+elif is_cold:
+    print('Cold Day')
+    print('Wear warm clothes')
+else:
+    print('Its a lovely day')
+
+print("Enjoy your day")
+
+
+is_hot = False
+
+if is_hot:
+    print("Hot Day")
+print("Enjoy your day")
