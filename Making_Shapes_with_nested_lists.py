a06ad2fa1c534d6fa1fe10144c1ae024a103f18c
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-12 22:44:46 +0530

    Create Making_Shapes_with_nested_lists.py

---
+++ Making_Shapes_with_nested_lists.py
@@ -0,0 +1,11 @@
+for item in [6,6,2,2,6,6,2,2]:
+        for x in [' x ']:
+            print(item*x)
+
+
+numbers = [5,2,5,2,2]
+for x_count in numbers:
+    output= ' '
+    for count in range(x_count):
+        output+='x'
+    print(output)
