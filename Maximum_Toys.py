026ddf5ce18b670fe850f388a83b2a69e9781943
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-15 22:57:55 +0530

    Create Maximum_Toys.py

---
+++ Maximum_Toys.py
@@ -0,0 +1,14 @@
+def MaximumToys(prices, k):
+    count = 0
+    cost = 0
+
+    for price in prices:
+        if cost+price <= k:
+            count +=1
+            cost+=price
+    return count
+
+n, k = [int(v) for v in input().split()]
+prices = sorted([int(v) for v in input().split()])
+res = MaximumToys(prices, k)
+print(res)
