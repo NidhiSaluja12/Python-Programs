180a97bf2c51d80422f5a0c54cc2cef2f4932153
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-29 22:27:18 +0530

    Create Staircase.py

---
+++ Staircase.py
@@ -0,0 +1,12 @@
+def staircase(n):
+    for i in range(1,n+1):
+        res=' '*(n-i)+ '#'*i
+        print(res)
+        
+  
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
+
