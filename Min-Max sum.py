92d816c89bbc4ad726a2af07c00bbf5e3f425f88
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-29 22:29:08 +0530

    Create Min-Max sum.py

---
+++ Min-Max sum.py
@@ -0,0 +1,17 @@
+def miniMaxSum(arr):
+    res = []
+    for i in range(len(arr)):
+        sum1 = sum(arr)-arr[i]
+        res.append(sum1)
+    min1 = min(res)
+    max1 = max(res)
+    print(min1,max1,sep=' ')
+        
+ 
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
+
