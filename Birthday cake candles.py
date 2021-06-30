1d6af124cbcecb38dcd913f40c6791e2b38a09bd
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-06-30 22:16:56 +0530

    Create Birthday cake candles.py

---
+++ Birthday cake candles.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def birthdayCakeCandles(candles):
+    count = 0
+    max1 = candles[0]
+    for number in candles:
+        if number>max1:
+            max1 = number
+            count = 1
+        elif number==max1:
+            count+=1
+            
+    return count
+        
+ 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
