9c8d8bc75aa968705254e0d7702050a683887d4c
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-07-08 18:33:05 +0530

    Create Minimum swaps.py

---
+++ Minimum swaps.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    
+    count =0
+    length = len(arr)
+    i = 0
+    while i<length:
+        index = arr[i]-1
+        if arr[i]!=arr[index]:
+            arr[i],arr[index]=arr[index],arr[i]
+            count+=1
+        else:
+            i+=1
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
+
