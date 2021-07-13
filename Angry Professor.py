e66507eb6585a960e9f45c30a64a2c98fcf006d5
NidhiSaluja12 <84669505+NidhiSaluja12@users.noreply.github.com>
2021-07-13 13:39:57 +0530

    Create Angry Professor.py

---
+++ Angry Professor.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def angryProfessor(k, a):
+    arrival_on_time = 0
+    arrival_late = 0
+    for i in range (len(a)):
+        if a[i] <= 0:
+            arrival_on_time += 1
+        elif a[i] > 0:
+            arrival_late += 1
+    if arrival_on_time < k:
+        return "YES"
+    else:
+        return "NO"
+    
+   
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
