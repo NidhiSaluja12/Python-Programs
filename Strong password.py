7cd01949434c0489e12c1e7879dcf3dd311614a3
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-07-22 23:09:51 +0530

    Create Strong password.py

---
+++ Strong password.py
@@ -0,0 +1,21 @@
+def strongPassword(n, password):
+    count = 0
+    if any(i.isdigit() for i in password) == False:
+        count+=1
+        
+    if any(i.isupper() for i in password) == False:
+        count+=1
+    
+    if any(i.islower() for i in password) == False:
+        count+=1
+        
+    if any(i in '!@#$%^&*()-+' for i in password) == False:
+        count+=1
+        
+    return max(count, 6-n)
+
+n = int(input())
+password = input()
+result = strongPassword(n, password)
+print(result)
+
