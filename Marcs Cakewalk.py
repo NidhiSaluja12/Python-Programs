3e75899c4f979cf49e10072685a40569818a6577
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-07-31 22:54:47 +0530

    Create Marcs Cakewalk.py

---
+++ Marcs Cakewalk.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    calorie.sort()
+    calorie.reverse()
+    result = 0
+    for i in range (len(calorie)):
+        res = calorie[i] * (2**i)
+        result += res
+        
+    return result
+    
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
