d39c596fa480eff6f7a74fa6b495f65fcc6fd702
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-01 23:19:06 +0530

    Create Luck Balance.py

---
+++ Luck Balance.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'luckBalance' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. 2D_INTEGER_ARRAY contests
+#
+
+def luckBalance(k, contests):
+    
+    contests.sort(reverse=True)
+    luck = 0
+
+    for contest in contests:
+        if contest[1] == 0:
+            luck += contest[0]
+        elif k > 0:
+            luck += contest[0]
+            k -= 1
+        else:
+            luck -= contest[0]
+
+    return luck
+                
+                
+    
+    
+    #return luck_balance
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    contests = []
+
+    for _ in range(n):
+        contests.append(list(map(int, input().rstrip().split())))
+
+    result = luckBalance(k, contests)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
