50800b1d53cdf5ed906c01e877cdb4a3d4feef6b
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-19 23:14:55 +0530

    Update CamelCase.py

--- CamelCase.py
+++ CamelCase.py
@@ -1,17 +1,9 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'camelcase' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts STRING s as parameter.
-#
 
 def camelcase(s):
     count = 0
@@ -20,8 +12,7 @@ def camelcase(s):
             count += 1
     return count+1
             
-    # Write your code here
-
+  
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
