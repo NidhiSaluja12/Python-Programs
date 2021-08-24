854a974ee7dfea4cfd050419f8f7683b8ab73257
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-24 22:55:52 +0530

    Create Rats count house.py

---
+++ Rats count house.py
@@ -0,0 +1,24 @@
+def ratCountHouse(rats, unit, array, size):
+    output = 0
+    totalFoodRequired = rats * unit
+    foodTillNow = 0
+    if size == 0:
+        return -1
+
+    for i in range(size):
+        foodTillNow += array[i]
+        output += 1
+        if foodTillNow >= totalFoodRequired:
+            return output
+
+    if totalFoodRequired > foodTillNow:
+        return 0
+
+
+r = int(input())
+unit = int(input())
+n = int(input())
+arr = list(map(int, input().split()))
+
+result = ratCountHouse(r, unit, arr, n)
+print(result)
