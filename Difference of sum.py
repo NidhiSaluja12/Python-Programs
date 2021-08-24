77618684a557d884271c1eb8c5f88adfdcab9b1f
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-24 23:03:55 +0530

    Create Difference of sum.py

---
+++ Difference of sum.py
@@ -0,0 +1,19 @@
+def differenceOfSum(n, m):
+    sum1 = 0
+    sum2 = 0
+
+    if n > 0 and m > 0:
+        for i in range(1, m + 1):
+            if i % n != 0:
+                sum1 += i
+            elif i % n == 0:
+                sum2 += i
+    difference = sum1 - sum2
+
+    return difference
+
+
+n = int(input())
+m = int(input())
+result = differenceOfSum(n, m)
+print(result)
