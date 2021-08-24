ba3ce85e31d57c56ef686a087b15030c08818638
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-24 23:25:54 +0530

    Create Password Checker.py

---
+++ Password Checker.py
@@ -0,0 +1,26 @@
+def CheckPassword(str, n):
+    output = 0
+    num = 0
+    cap = 0
+    if n < 4:
+        output = 0
+    if str[0].isdigit():
+        output = 0
+
+    for i in str:
+        if i == '/':
+            output = 0
+        if i.isdigit():
+            num += 1
+        if i.isupper():
+            cap += 1
+    if cap >= 1 and num >= 1:
+        output = 1
+
+
+    return output
+
+str = input()
+n = len(str)
+result = CheckPassword(str, n)
+print(result)
