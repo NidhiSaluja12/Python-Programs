33659c2ed95f442678476fd0fe68f98204a5b411
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-08-31 23:34:32 +0530

    Update Migratory birds.py

--- Migratory birds.py
+++ Migratory birds.py
@@ -6,14 +6,8 @@ import random
 import re
 import sys
 
-#
-# Complete the 'migratoryBirds' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
-
 def migratoryBirds(arr):
+    
     arr.sort()
     max1 = 1
     count = 1
