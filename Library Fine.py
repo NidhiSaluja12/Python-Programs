7be87154aac5b7d346afe419aa495f62d8abe755
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-04 23:09:03 +0530

    Update Library Fine.py

--- Library Fine.py
+++ Library Fine.py
@@ -1,4 +1,10 @@
-#!/bin/python3
+'''
+Sample Input
+9 6 2015
+6 6 2015
+
+Sample Output
+45'''
 
 import math
 import os
@@ -6,18 +12,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'libraryFine' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER d1
-#  2. INTEGER m1
-#  3. INTEGER y1
-#  4. INTEGER d2
-#  5. INTEGER m2
-#  6. INTEGER y2
-#
 
 def libraryFine(d1, m1, y1, d2, m2, y2):
     fine = 0
@@ -33,9 +27,6 @@ def libraryFine(d1, m1, y1, d2, m2, y2):
                 fine = 0
     return fine
     
-            
-        
-    # Write your code here
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
