943b33e6c748e2e94bc49cf0b9a0c5e731ec1d22
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-08 22:05:11 +0530

    Update Gemstone.py

--- Gemstone.py
+++ Gemstone.py
@@ -1,28 +1,21 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'gemstones' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts STRING_ARRAY arr as parameter.
-#
 
 def gemstones(arr):
+    
     res = set(arr[0])
     
     for i in range(1, len(arr)):
         temp = set(arr[i])
         res = res.intersection(temp)
+        
     return len(res)
     
-    
-    # Write your code here
+   
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
