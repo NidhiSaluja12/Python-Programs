adde6d3023ee5581e719bca8a653f4f1e3f8589b
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-09 23:52:14 +0530

    Update Move hyphens to front.py

--- Move hyphens to front.py
+++ Move hyphens to front.py
@@ -1,11 +1,15 @@
 def moveHyphens(str):
+    
     count = 0
     final = ""
+    
     for i in str:
         if i == '-':
             count+=1
+            
         else:
             final+=i
+            
     return print("-"*count,final)
 
 
