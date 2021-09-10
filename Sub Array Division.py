56c9a3d3ed51e1d8cdce4e3bcfec50b35d3e8221
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-10 23:07:31 +0530

    Update Sub Array Division.py

--- Sub Array Division.py
+++ Sub Array Division.py
@@ -5,10 +5,12 @@ import re
 import sys
 
 def birthday(s, d, m):
+    
     count = 0
     for i in range (len(s)):
         if sum(s[i:i+m])==d:
             count+=1
+            
     return count
   
 if __name__ == '__main__':
