754e4670ac5e212c5db4bfe406ce378bb8267e8d
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-11 22:54:50 +0530

    Update Palindrome(string).py

--- Palindrome(string).py
+++ Palindrome(string).py
@@ -1,11 +1,15 @@
 string = input()
 st_len = len(string)
 temp = False
+
+
 for i in range(int(st_len/2)):
     if string[i] == string[st_len - i - 1]:
         temp = True
+        
 
 if temp == True:
     print("Palindrome")
+    
 else:
     print("Not palindrome")
