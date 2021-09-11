61787a9739c4af37041a05376605e91a730c318b
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-11 22:54:05 +0530

    Update Palindrome(number).py

--- Palindrome(number).py
+++ Palindrome(number).py
@@ -2,10 +2,12 @@ number = int(input("Enter the number:\n"))
 temp = number
 reverse = 0
 
+
 while (number > 0):
     remainder = number % 10
     reverse = reverse * 10 + remainder
     number = number // 10
+    
 
 if (temp == reverse):
     print("Palindrome")
