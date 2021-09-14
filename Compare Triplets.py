98711c4e1d8fd935bdd5e21b8b54651771916eba
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-14 20:40:26 +0530

    Update Compare Triplets.py

--- Compare Triplets.py
+++ Compare Triplets.py
@@ -1,13 +1,16 @@
 def compareTriplets(a, b):
+    
     A_length = len(a)
     B_length = len(b)
     A_points = 0
     B_points = 0
+    
     for i in range(A_length):
         if a[i]>b[i]:
             A_points+=1
         elif a[i]<b[i]:
             B_points+=1
+            
     return A_points,B_points
             
   
