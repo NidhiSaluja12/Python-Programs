ae17df09503ede9adac1c2c0d3b19aad5e8823f9
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-15 21:31:51 +0530

    Update Pangrams.py

--- Pangrams.py
+++ Pangrams.py
@@ -1,17 +1,8 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'pangrams' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING s as parameter.
-#
 
 def pangrams(s):
     # s = set(s)
@@ -22,7 +13,7 @@ def pangrams(s):
     # return "not pangram"
 
     return 'pangram' if len(set(s.lower()))==27 else 'not pangram'
-    # Write your code here
+   
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
