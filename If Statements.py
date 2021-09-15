654b8f7c7abae9af1cb013dd0f61b278c5b37e1c
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-15 21:32:22 +0530

    Update If Statements.py

--- If Statements.py
+++ If Statements.py
@@ -1,12 +1,15 @@
 is_hot = False
 is_cold= False
 
+
 if is_hot:
     print("Hot Day")
     print("Drink water.")
+    
 elif is_cold:
     print('Cold Day')
     print('Wear warm clothes')
+    
 else:
     print('Its a lovely day')
 
