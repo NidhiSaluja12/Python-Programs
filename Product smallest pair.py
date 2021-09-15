353d659b87aff9f87dee84c6c520cb69193c117a
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-15 21:33:07 +0530

    Update Product smallest pair.py

--- Product smallest pair.py
+++ Product smallest pair.py
@@ -1,9 +1,11 @@
 def ProductSmallestPair(sum, arr):
     n = len(arr)
+    
     if n < 2:
         return -1
 
     arr = sorted(arr)
+    
     for i in range(n - 1):
         if arr[i] + arr[i + 1] <= sum:
             product = arr[i] * arr[i + 1]
