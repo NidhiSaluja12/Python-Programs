4ec12ffca635161099d67de6c0c0c8f23c249239
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-15 21:33:56 +0530

    Update pairs.py

--- pairs.py
+++ pairs.py
@@ -1,29 +1,20 @@
-#!/bin/python
-
 import math
 import os
 import random
 import re
 import sys
 
-#
-# Complete the 'pairs' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER k
-#  2. INTEGER_ARRAY arr
-#
-
 def pairs(k, arr):
+    
     arr = set(arr)
     pairs = 0
+    
     for i in arr:
         if i+k in arr:
             pairs += 1
                 
     return pairs
-    # Write your code here
+ e
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
