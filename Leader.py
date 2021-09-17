8fa0278b10201ba6e779fa871b2d300ba2896208
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-17 17:58:57 +0530

    Create Leader.py

---
+++ Leader.py
@@ -0,0 +1,13 @@
+def leader(arr):
+    sum = 0
+    for i in range(len(arr)-1):
+        for j in range(i+1, len(arr)):
+            if arr[i] >= arr[j]:
+                leader1 = arr[i]
+
+
+
+    return leader1+arr[len(arr)-1]
+
+arr = list(map(int, input().split()))
+print(leader(arr))
