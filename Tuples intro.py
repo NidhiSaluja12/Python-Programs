68e057ffd5f6fb2213cbab75b8a36111545574c4
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-09-28 09:06:31 +0530

    Update Tuples intro.py

--- Tuples intro.py
+++ Tuples intro.py
@@ -1,2 +1,4 @@
+# Tuples - Tuples are the immutable datatypes in python used to store elements. Even being immutable they are used because of memory efficiency.
+
 numbers = (1,2,3)
 print(numbers[0])
