f0b47ef0f31297bc0896ad4a60737f9365371c12
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-05-12 23:18:49 +0530

    Update Armstrong_number.py

--- Armstrong_number.py
+++ Armstrong_number.py
@@ -7,7 +7,6 @@ while (number > 0):
     digits = number % 10                                         # 153%10 = 3 ; 15%10 = 5 ; 1%10 = 1
     sum += digits ** order                                       # 1*1*1 + 5*5*5 + 3*3*3
     number = number // 10                                        # 153//10=15 ; 15//10=1 ; 1//10=0
-
 if (sum == copy_of_number):
     print(f"{copy_of_number} is an armstrong number")
 
