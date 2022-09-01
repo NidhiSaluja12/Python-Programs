9ed5d9309c104da4ae4d8021653dfdaf584e905c
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-09-01 23:26:48 +0530

    Update Apple and Orange.py

--- Apple and Orange.py
+++ Apple and Orange.py
@@ -5,6 +5,7 @@ import re
 import sys
 
 
+
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     print(sum([1 for x in apples if (x + a) >= s and (x + a) <= t]))
     print(sum([1 for x in oranges if (x + b) >= s and (x + b) <= t]))
