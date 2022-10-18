8d6dd31f442a354b564e6ff0047596376e5e8661
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-10-18 23:53:54 +0530

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,6 +1,5 @@
 # to implement binary search, array should be sorted.
-def Binary_search(item, list1):
-    
+def Binary_search(item, list1):   
  
    lower_bound = 0
     upper_bound = len(list1) - 1
